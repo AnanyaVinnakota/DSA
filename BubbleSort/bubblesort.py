2a45f552763011b8bc2370ee6c037d44319e9709
unknown <vinnakota.ananya@gmail.com>
2026-05-23 17:54:35 +0530

    fix: resolved syntax error in bubble sort

--- BubbleSort/bubblesort.py
+++ BubbleSort/bubblesort.py
@@ -1,10 +1,11 @@
 def bubblesort(arr):
     n=len(arr)
     for i in range(n):
-        for j in range(0,n-i-1);
+        for j in range(0,n-i-1):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
     return arr
-arr=list(map(int,input("Enter the elements: ").split()))
-sorted_arr=bubblesort(arr)
-print("Sorted array: ",sorted_arr)
+if __name__ == "__main__":
+    arr=list(map(int,input("Enter the elements: ").split()))
+    sorted_arr=bubblesort(arr)
+    print("Sorted array: ",sorted_arr)
